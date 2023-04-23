ed2c3a74152ebcbcd042b4831d3f7f9ad2082db5
Soe Htet Linn <mgsoehtetlinn@gmail.com>
2023-04-24 02:41:38 +0630

    testing for sw.

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -21,6 +21,8 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
+  console.log("devorprod",process.env.NODE_ENV)
+  console.log("navigator",navigator)
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
